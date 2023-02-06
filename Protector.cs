438d6280b375a3a4f58c1651beb12823f2dbc88f
In-tillel <t50_t.j.jenishbekov@mpt.ru>
2023-02-06 13:50:17 +0300

    Добавьте файлы проекта.

---
+++ Protector.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+
+namespace _10_praktika
+{
+    internal class Protector
+    {
+        private static Dictionary<string, User> Users = new Dictionary<string, User>();
+
+
+        public static User Register(string username, string password, string[] roles = null)
+        {
+
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+
+
+            var saltedhashedPassword = SaltAndHashPassword(password, saltText);
+            var user = new User
+            {
+                Name = username,
+                Salt = saltText,
+                SaltedHashedPassword = saltedhashedPassword,
+                Roles = roles
+            };
+
+            Users.Add(user.Name, user);
+            return user;
+        }
+
+        private static string SaltAndHashPassword(string password, string salt)
+        {
+            var sha = SHA256.Create();
+            var saltedPassword = password + salt;
+            return Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
+
+
+        public static void LogIn(string username, string password)
+        {
+            if (CheckPassword(username, password))
+            {
+                var identity = new GenericIdentity(username, "CompanyAuth");
+                var principal = new GenericPrincipal(identity, Users[username].Roles);
+                System.Threading.Thread.CurrentPrincipal = principal;
+            }
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+            if (!Users.ContainsKey(username))
+            {
+                return false;
+            }
+
+            var user = Users[username];
+
+
+            var saltedhashedPassword = SaltAndHashPassword(password, user.Salt);
+            return (saltedhashedPassword == user.SaltedHashedPassword);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10_praktika
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Salt { get; set; }
+        public string SaltedHashedPassword { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
